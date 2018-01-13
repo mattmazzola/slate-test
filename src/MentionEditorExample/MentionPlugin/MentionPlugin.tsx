a1b08616ddde34e548428c97c7622e4461079533
Matt Mazzola <mattm@microsoft.com>
2018-01-12 16:52:01 -0800

    Move functions to utilities

--- src/MentionEditorExample/MentionPlugin/MentionPlugin.tsx
+++ src/MentionEditorExample/MentionPlugin/MentionPlugin.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as immutable from 'immutable'
 import { IPickerProps, NodeTypes } from './models'
+import { getNodesByPath, findNodeByPath } from './utilities'
 import MentionNode from './MentionNode'
 import './MentionPlugin.css'
 
@@ -22,44 +23,6 @@ const defaultOptions: IOptions = {
     triggerCharacter: '$',
     closingCharacter: ' ',
     onChangeMenuProps: () => { },
-}
-
-const findNodeByPath = (path: number[], root: any, nodeType: string = NodeTypes.Mention): any => {
-    if (path.length === 0) {
-        return null
-    }
-
-    const [nextKey, ...nextPath] = path
-
-    const nextRoot = root.findDescendant((node: any, i: number) => i === nextKey)
-    // If the node was already removed due to another change it might not exist in the path anymore
-    if (nextRoot === null) {
-        return null
-    }
-
-    if (nextRoot.type === nodeType) {
-        return nextRoot
-    }
-
-    return findNodeByPath(nextPath, nextRoot)
-}
-
-const getNodesByPath = (path: number[], root: any, nodes: any[] = []): any[] => {
-    if (path.length === 0) {
-        return nodes
-    }
-
-    const [nextKey, ...nextPath] = path
-    const nextRoot = root.findDescendant((node: any, i: number) => i === nextKey)
-
-    // If the node was already removed due to another change it might not exist in the path anymore
-    if (nextRoot === null) {
-        return nodes
-    }
-
-    nodes.push(nextRoot)
-
-    return getNodesByPath(nextPath, nextRoot, nodes)
 }
 
 export default function mentionPlugin(inputOptions: Partial<IOptions> = {}) {
--- src/MentionEditorExample/MentionPlugin/utilities.ts
+++ src/MentionEditorExample/MentionPlugin/utilities.ts
@@ -1,3 +1,5 @@
+import { NodeTypes } from "./models"
+
 /**
  * Recursively walk up DOM tree until root or parent with non-static position is found.
  * (relative, fixed, or absolute) which will be used as reference for absolutely positioned elements within it
@@ -29,3 +31,71 @@ export const valueToJSON = (value: any) => {
         selection: value.selection.toJSON()
     }
 }
+
+
+export const findNodeByPath = (path: number[], root: any, nodeType: string = NodeTypes.Mention): any => {
+    if (path.length === 0) {
+        return null
+    }
+
+    const [nextKey, ...nextPath] = path
+
+    const nextRoot = root.findDescendant((node: any, i: number) => i === nextKey)
+    // If the node was already removed due to another change it might not exist in the path anymore
+    if (nextRoot === null) {
+        return null
+    }
+
+    if (nextRoot.type === nodeType) {
+        return nextRoot
+    }
+
+    return findNodeByPath(nextPath, nextRoot)
+}
+
+export const getNodesByPath = (path: number[], root: any, nodes: any[] = []): any[] => {
+    if (path.length === 0) {
+        return nodes
+    }
+
+    const [nextKey, ...nextPath] = path
+    const nextRoot = root.findDescendant((node: any, i: number) => i === nextKey)
+
+    // If the node was already removed due to another change it might not exist in the path anymore
+    if (nextRoot === null) {
+        return nodes
+    }
+
+    nodes.push(nextRoot)
+
+    return getNodesByPath(nextPath, nextRoot, nodes)
+}
+
+export const getEntitiesFromValue = (value: any) => {
+    const tree = value.toJSON().document
+
+    return depthFirstSearch(tree, n => n.type === NodeTypes.Mention && n.data.completed === true, n => n.type === NodeTypes.Optional)
+}
+
+interface INode {
+    kind: string
+    type: string
+    nodes: INode[] | undefined
+    data: any
+}
+
+const depthFirstSearch = (root: INode, predicate: (n: INode) => boolean, exclude: (n: INode) => boolean, nodes: INode[] = []): INode[] => {
+    if (predicate(root)) {
+        nodes.push(root)
+    }
+
+    const childNodes = !Array.isArray(root.nodes)
+        ? []
+        : root.nodes
+            .filter(n => !exclude(n))
+            .map(n => depthFirstSearch(n, predicate, exclude))
+            .reduce((a, b) => [...a, ...b], [])
+
+    return [...nodes, ...childNodes]
+}
+
